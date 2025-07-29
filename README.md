42e647a9cbe9a37001bc5f5c644d87a28104bd2e
Sergey Kosykh <s3rkosal@gmail.com>
2025-07-29 15:34:47 +0400

    update files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # intfs
 Interactive file listing
+
+## Planed feautures
+
+- 1. limit displayed files, to avoid visual cluttering.
+- 2. expand/collaps directories by just clicking on them.
+- 3. utility remembers settings for each launched directory.
+- 4. updates whenever files changed using inotify.
+- 5. fuzzy search.
+- 6. maximum integration with external utilities & programs such as tmux, vim, helix editor.
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <filesystem>
 
 int main() {
+  std::filesystem::path dirpath = ".";
+
+  if (std::filesystem::exists(dirpath)
+      && std::filesystem::is_directory(dirpath))
+    for (const auto& entry : std::filesystem::directory_iterator(dirpath))
+    {        
+      std::cout << "File: " << entry.path() << "\n";
+      
+    }
+
+  else
+    std::cerr << "Directory not found!\n";
+  
   std::cout << "Initial commit\n";
   return 0;
 }
